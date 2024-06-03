f3e0a306ce7213e89b2b5ba24c7153c072390dc9
Murali krishna sai Chukka <murali.24.4.91@gmail.com>
2024-06-03 04:49:44 +0000

    remove commented code

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const db = require("./db/db");
-//const { readdirSync } = require("fs");
 const api = require('./routes/transactions');
 
 require("dotenv").config();
 
 const app = express();
 
-
-
 //middleware
 app.use(express.json());
 app.use(cors());
-
-// //routes
-// readdirSync("./routes").map((route) => {
-//   app.use("/api/v1", require("./routes/" + route));
-// });
 
 app.use('/api/v1', api);
 
