42ccc7fa61477b51288431ab142e3f1c6a490e1a
Murali krishna sai Chukka <murali.24.4.91@gmail.com>
2024-06-03 21:49:47 -0500

    Update income.js
    Updated income for account changes

--- frontend/src/components/income/income.js
+++ frontend/src/components/income/income.js
@@ -1,50 +1,71 @@
 import React, { useState } from "react";
 import { useGlobalContext } from "../../context/globalContext";
-import { DataView } from 'primereact/dataview';
-import { Button } from 'primereact/button';
-import { TransactionForm } from '../transaction/transactionForm';
-import { TransactionBlock } from '../transaction/transactionBlock';
-import { getCode } from '../../utils/utilites';
+import { DataView } from "primereact/dataview";
+import { Button } from "primereact/button";
+import { TransactionForm } from "../transaction/transactionForm";
+import { TransactionBlock } from "../transaction/transactionBlock";
+import { getCode } from "../../utils/utilites";
 
 function Income() {
-  const { incomes, addIncome, deleteIncome, updateIncome, totalIncome, getTransactionsCategories } = useGlobalContext();
+  const {
+    incomes,
+    addIncome,
+    deleteIncome,
+    updateIncome,
+    totalIncome,
+    getTransactionsCategories,
+  } = useGlobalContext();
   const [visible, setVisible] = useState(false);
   const [income, setIncome] = useState(null);
-  const [mode, setMode] = useState('Add');
+  const [mode, setMode] = useState("Add");
 
-  const listTemplate = ((items) => {
+  const listTemplate = (items) => {
     if (!items || items.length === 0) return null;
 
     let list = items.map((income) => {
-      return <TransactionBlock
-        transaction={income}
-        deleteTransaction={deleteIncome}
-        updateTransaction={onUpdate}
-        key={income._id}
-        type={"income"}
-        categories={getTransactionsCategories('incomes', 'category')}
-        subCategories={getTransactionsCategories('incomes', 'subCategory', 'category')} />;
+      return (
+        <TransactionBlock
+          transaction={income}
+          deleteTransaction={deleteIncome}
+          updateTransaction={onUpdate}
+          key={income._id}
+          type={"income"}
+          categories={getTransactionsCategories("incomes", "category")}
+          subCategories={getTransactionsCategories(
+            "incomes",
+            "subCategory",
+            "category"
+          )}
+        />
+      );
     });
 
     return <div className="grid grid-nogutter">{list}</div>;
-  });
+  };
 
   const onUpdate = (income) => {
     setIncome(income);
     setMode("Update");
     setVisible(true);
-  }
+  };
 
   const onSubmit = (incomeDetails) => {
     incomeDetails = {
       ...incomeDetails,
-      category: typeof incomeDetails.category === "string" ?
-        getCode(incomeDetails.category) :
-        incomeDetails.category.code, date: new Date(incomeDetails.date).toLocaleDateString(),
-      subCategory: typeof incomeDetails.subCategory === "string" ?
-        getCode(incomeDetails.subCategory) :
-        incomeDetails.subCategory.code,
-    }
+      category:
+        typeof incomeDetails.category === "string"
+          ? getCode(incomeDetails.category)
+          : incomeDetails.category.code,
+      date: new Date(incomeDetails.date).toLocaleDateString(),
+      subCategory:
+        typeof incomeDetails.subCategory === "string"
+          ? getCode(incomeDetails.subCategory)
+          : incomeDetails.subCategory.code,
+      account:
+        typeof incomeDetails.account === "string"
+          ? getCode(incomeDetails.account)
+          : incomeDetails.account.code,
+    };
     if (incomeDetails._id) {
       updateIncome(incomeDetails._id, incomeDetails);
       setIncome(null);
@@ -53,12 +74,12 @@ function Income() {
     }
     setMode("Add");
     setVisible(false);
-  }
+  };
 
   const onCancel = () => {
     setIncome(null);
     setVisible(false);
-  }
+  };
   return (
     <>
       <h1>Total Income:{totalIncome()}</h1>
@@ -71,10 +92,16 @@ function Income() {
         onSubmit={onSubmit}
         onCancel={onCancel}
         mode={mode}
-        categoryOptions={getTransactionsCategories('incomes', 'category')}
-        subCategoryOptions={getTransactionsCategories('incomes', 'subCategory', 'category')}
+        categoryOptions={getTransactionsCategories("incomes", "category")}
+        subCategoryOptions={getTransactionsCategories(
+          "incomes",
+          "subCategory",
+          "category"
+        )}
+        accountOptions={getTransactionsCategories("incomes", "account")}
         type={"incomes"}
-        transaction={income} />
+        transaction={income}
+      />
       <Button
         icon="pi pi-plus"
         className="fixed add-icon"
@@ -83,8 +110,9 @@ function Income() {
         aria-label="Add"
         onClick={() => {
           setMode("Add");
-          setVisible(!visible)
-        }} />
+          setVisible(!visible);
+        }}
+      />
     </>
   );
 }
