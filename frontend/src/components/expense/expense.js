86870e1c946eecd27565690b60ee31252531518d
Murali krishna sai Chukka <murali.24.4.91@gmail.com>
2024-06-03 21:51:36 -0500

    Update expense.js
    Updated expense based on account

--- frontend/src/components/expense/expense.js
+++ frontend/src/components/expense/expense.js
@@ -1,54 +1,72 @@
 import React, { useState } from "react";
 import { useGlobalContext } from "../../context/globalContext";
-import { DataView } from 'primereact/dataview';
-import { Button } from 'primereact/button';
-import { TransactionForm } from '../transaction/transactionForm';
-import { TransactionBlock } from '../transaction/transactionBlock';
-import { getCode } from '../../utils/utilites';
+import { DataView } from "primereact/dataview";
+import { Button } from "primereact/button";
+import { TransactionForm } from "../transaction/transactionForm";
+import { TransactionBlock } from "../transaction/transactionBlock";
+import { getCode } from "../../utils/utilites";
 
 function Expense() {
-  const { expenses, addExpense, updateExpense, getTransactionsCategories, deleteExpense, totalExpenses } = useGlobalContext();
+  const {
+    expenses,
+    addExpense,
+    updateExpense,
+    getTransactionsCategories,
+    deleteExpense,
+    totalExpenses,
+  } = useGlobalContext();
 
   const [visible, setVisible] = useState(false);
   const [expense, setExpense] = useState(null);
-  const [mode, setMode] = useState('Add');
+  const [mode, setMode] = useState("Add");
 
-  const listTemplate = ((items) => {
+  const listTemplate = (items) => {
     if (!items || items.length === 0) return null;
 
     let list = items.map((expense) => {
-      return <TransactionBlock
-        transaction={expense}
-        deleteTransaction={deleteExpense}
-        updateTransaction={onUpdate}
-        key={expense._id}
-        type={"expense"}
-        categories={getTransactionsCategories('expenses', 'category')}
-        subCategories={getTransactionsCategories('expenses', 'subCategory', 'category')} />;
+      return (
+        <TransactionBlock
+          transaction={expense}
+          deleteTransaction={deleteExpense}
+          updateTransaction={onUpdate}
+          key={expense._id}
+          type={"expense"}
+          categories={getTransactionsCategories("expenses", "category")}
+          subCategories={getTransactionsCategories(
+            "expenses",
+            "subCategory",
+            "category"
+          )}
+        />
+      );
     });
 
     return <div className="grid grid-nogutter">{list}</div>;
-  });
+  };
 
   const onUpdate = (expense) => {
     setExpense(expense);
     setMode("Update");
     setVisible(true);
-  }
+  };
 
   const onSubmit = (expenseDetails) => {
     expenseDetails = {
       ...expenseDetails,
-      category: typeof expenseDetails.category === "string" ?
-        getCode(expenseDetails.category) :
-        expenseDetails.category.code, date: new Date(expenseDetails.date).toLocaleDateString(),
-      subCategory: typeof expenseDetails.subCategory === "string" ?
-        getCode(expenseDetails.subCategory) :
-        expenseDetails.subCategory.code,
-      paidBy: typeof expenseDetails.paidBy === "string" ?
-        getCode(expenseDetails.paidBy) :
-        expenseDetails.paidBy.code,
-    }
+      category:
+        typeof expenseDetails.category === "string"
+          ? getCode(expenseDetails.category)
+          : expenseDetails.category.code,
+      date: new Date(expenseDetails.date).toLocaleDateString(),
+      subCategory:
+        typeof expenseDetails.subCategory === "string"
+          ? getCode(expenseDetails.subCategory)
+          : expenseDetails.subCategory.code,
+      account:
+        typeof expenseDetails.account === "string"
+          ? getCode(expenseDetails.account)
+          : expenseDetails.account.code,
+    };
     if (expenseDetails._id) {
       updateExpense(expenseDetails._id, expenseDetails);
       setExpense(null);
@@ -57,12 +75,12 @@ function Expense() {
     }
     setMode("Add");
     setVisible(false);
-  }
+  };
 
   const onCancel = () => {
     setExpense(null);
     setVisible(false);
-  }
+  };
 
   return (
     <>
@@ -76,21 +94,27 @@ function Expense() {
         onSubmit={onSubmit}
         onCancel={onCancel}
         mode={mode}
-        categoryOptions={getTransactionsCategories('expenses', 'category')}
-        subCategoryOptions={getTransactionsCategories('expenses', 'subCategory', 'category')}
-        paidByOptions={getTransactionsCategories('expenses', 'paidBy')}
+        categoryOptions={getTransactionsCategories("expenses", "category")}
+        subCategoryOptions={getTransactionsCategories(
+          "expenses",
+          "subCategory",
+          "category"
+        )}
+        accountOptions={getTransactionsCategories("expenses", "account")}
         type={"expenses"}
-        transaction={expense} />
+        transaction={expense}
+      />
       <Button
         icon="pi pi-plus"
         className="fixed add-icon"
         rounded
         severity="info"
         aria-label="Add"
-        onClick={() =>{
+        onClick={() => {
           setMode("Add");
-          setVisible(!visible)
-        }} />
+          setVisible(!visible);
+        }}
+      />
     </>
   );
 }
