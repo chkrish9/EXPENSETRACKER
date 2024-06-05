6b0e2223d25e294cc199b1ea1b24e14257430ca4
Murali krishna sai Chukka <murali.24.4.91@gmail.com>
2024-06-04 23:44:49 -0500

    Update transactions.js
    Updating routes

--- backend/routes/transactions.js
+++ backend/routes/transactions.js
@@ -7,11 +7,11 @@ const router = require('express').Router();
 router.use(verifyJWT);
 
 router.post('/add-income', addIncome)
-    .get('/get-incomes/:user', getIncomes)
+    .get('/get-incomes', getIncomes)
     .put('/update-income/:id',updateIncome)
     .delete('/delete-income/:id', deleteIncome)
     .post('/add-expense', addExpense)
-    .get('/get-expenses/:user', getExpenses)
+    .get('/get-expenses', getExpenses)
     .put('/update-expense/:id',updateExpense)
     .delete('/delete-expense/:id', deleteExpense);
 
