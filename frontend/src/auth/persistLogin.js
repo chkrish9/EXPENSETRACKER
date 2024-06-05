c7bf1013b91519ca23c2a92f19a672f29139d2c9
Murali krishna sai Chukka <murali.24.4.91@gmail.com>
2024-06-04 23:17:13 -0500

    Update persistLogin.js
    Updated refresh token

--- frontend/src/auth/persistLogin.js
+++ frontend/src/auth/persistLogin.js
@@ -1,76 +1,67 @@
-import { Outlet, Link } from "react-router-dom"
-import { useEffect, useRef, useState } from 'react'
+import { Outlet, Link } from "react-router-dom";
+import { useRef } from "react";
 import usePersist from "../hooks/usePersist";
 import { useGlobalContext } from "../context/globalContext";
-import { Messages } from 'primereact/messages';
-import { useMountEffect } from 'primereact/hooks';
+import { Messages } from "primereact/messages";
+import { useMountEffect } from "primereact/hooks";
 
 const PersistLogin = () => {
+  const [persist] = usePersist();
+  const { token } = useGlobalContext();
+  const msgs = useRef(null);
+  const { refresh, setToken } = useGlobalContext();
 
-    const [persist] = usePersist()
-    const { token } = useGlobalContext();
-    const effectRan = useRef(false)
-    const msgs = useRef(null);
-
-    const [trueSuccess, setTrueSuccess] = useState(false)
-
-    const { refresh, setToken } = useGlobalContext();
-
-    useEffect(() => {
-
-        if (effectRan.current === true) { // React 18 Strict Mode
-
-            const verifyRefreshToken = async () => {
-                console.log('verifying refresh token')
-                try {
-                    const response = await refresh()
-                    if (!response) {
-                        return;
-                    }
-                    const accessToken  = response;
-                    setToken(accessToken);
-                    setTrueSuccess(true)
-                }
-                catch (err) {
-                    console.error(err)
-                }
-                return;   
-            }
-
-            if (!token) verifyRefreshToken();
-        }
-
-        return () => effectRan.current = true
-    }, [])
-
-    useMountEffect(() => {
-        if (msgs.current) {
-            msgs.current.clear();
-            msgs.current.show([
-                { sticky: true, severity: 'error', summary: 'Error', detail: 'Some thing went wrong. Please login again', closable: false }
-            ]);
-        }
-    });
-
-
-    let content
-    if (!persist && token) { // persist: no
-        content = <Outlet />
-    } else if (trueSuccess) { //persist: yes, token: yes
-        console.log('success')
-        content = <Outlet />
-    } else if (token) { //persist: yes, token: yes
-        console.log('token and uninit')
-        content = <Outlet />
-    } else {
-        content = (
-            <>
-                <Messages ref={msgs} />
-                <Link to="/login">Login</Link>
-            </>
-        )
+  const errorContent = () => {
+    return (
+      <>
+        <Messages ref={msgs} />
+        <Link to="/login">Login</Link>
+      </>
+    );
+  };
+  const verifyRefreshToken = async () => {
+    console.log("verifying refresh token");
+    try {
+      const response = await refresh();
+      if (!response) {
+        return errorContent();
+      }
+      const accessToken = response;
+      setToken(accessToken);
+      return <Outlet />;
+    } catch (err) {
+      console.error(err);
+      return errorContent();
     }
+  };
 
-    return content
-}
-export default PersistLogin
+  useMountEffect(() => {
+    if (msgs.current) {
+      msgs.current.clear();
+      msgs.current.show([
+        {
+          sticky: true,
+          severity: "error",
+          summary: "Error",
+          detail: "Some thing went wrong. Please login again",
+          closable: false,
+        },
+      ]);
+    }
+  });
+
+  let content;
+  if (!persist && token) {
+    // persist: no
+    content = <Outlet />;
+  } else if (token) {
+    //persist: yes, token: yes
+    console.log("token and uninit");
+    content = <Outlet />;
+  } else {
+    verifyRefreshToken();
+  }
+
+  return content;
+};
+export default PersistLogin;
