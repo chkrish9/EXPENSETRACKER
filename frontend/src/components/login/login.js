8fdcc6cff28755b170ca13c479aa2fa7077870b7
Murali krishna sai Chukka <murali.24.4.91@gmail.com>
2024-06-04 22:44:36 -0500

    Update login.js
    wrong file updated in user

--- frontend/src/components/login/login.js
+++ frontend/src/components/login/login.js
@@ -1,76 +1,72 @@
-import { Outlet, Link } from "react-router-dom"
-import { useEffect, useRef, useState } from 'react'
-import usePersist from "../hooks/usePersist";
-import { useGlobalContext } from "../context/globalContext";
-import { Messages } from 'primereact/messages';
-import { useMountEffect } from 'primereact/hooks';
+import React, { useState, useRef } from 'react';
+import { Card } from 'primereact/card';
+import logo from '../../img/logo.png';
+import { InputText } from "primereact/inputtext";
+import { Password } from 'primereact/password';
+import { FloatLabel } from "primereact/floatlabel";
+import { Button } from 'primereact/button';
+import { useGlobalContext } from '../../context/globalContext';
+import { Toast } from 'primereact/toast';
+import { useNavigate } from 'react-router-dom'
 
-const PersistLogin = () => {
 
-    const [persist] = usePersist()
-    const { token } = useGlobalContext();
-    const effectRan = useRef(false)
-    const msgs = useRef(null);
-
-    const [trueSuccess, setTrueSuccess] = useState(false)
-
-    const { refresh, setToken } = useGlobalContext();
-
-    useEffect(() => {
-
-        if (effectRan.current === true) { // React 18 Strict Mode
-
-            const verifyRefreshToken = async () => {
-                console.log('verifying refresh token')
-                try {
-                    const response = await refresh()
-                    if (!response) {
-                        return;
-                    }
-                    const accessToken  = response;
-                    setToken(accessToken);
-                    setTrueSuccess(true)
-                }
-                catch (err) {
-                    console.error(err)
-                }
-                return;   
-            }
-
-            if (!token) verifyRefreshToken();
-        }
-
-        return () => effectRan.current = true
-    }, [])
-
-    useMountEffect(() => {
-        if (msgs.current) {
-            msgs.current.clear();
-            msgs.current.show([
-                { sticky: true, severity: 'error', summary: 'Error', detail: 'Some thing went wrong. Please login again', closable: false }
-            ]);
-        }
+export const Login = () => {
+    const [loginUser, setLoginUser] = useState({
+        username: '',
+        password: ''
     });
+    const navigate = useNavigate();
+    const toast = useRef(null);
 
+    const { setUser, login, setToken } = useGlobalContext();
 
-    let content
-    if (!persist && token) { // persist: no
-        content = <Outlet />
-    } else if (trueSuccess) { //persist: yes, token: yes
-        console.log('success')
-        content = <Outlet />
-    } else if (token) { //persist: yes, token: yes
-        console.log('token and uninit')
-        content = <Outlet />
-    } else {
-        content = (
-            <>
-                <Messages ref={msgs} />
-                <Link to="/login">Login</Link>
-            </>
-        )
+    const onLogin = async () => {
+        if (!loginUser.username || !loginUser.password) {
+            toast.current.show({ severity: 'error', summary: 'Error', detail: 'Please enter Username and Password', life: 3000 });
+            return;
+        }
+        try {
+            const response = await login(loginUser);
+            setToken(response.data.accessToken);
+            localStorage.setItem("username", loginUser.username)
+            setUser(loginUser.username);
+            setLoginUser({
+                username: '',
+                password: ''
+            })
+            navigate('/dashboard')
+        } catch (error) {
+            toast.current.show({ severity: 'error', summary: 'Error', detail: 'Wrong Username or Password', life: 3000 });
+        }
     }
-
-    return content
+    return (
+        <>
+            <div className="flex h-full flex-column justify-content-center align-items-center">
+                <div className="col-10 lg:col-6 xl:col-3">
+                    <div className="card">
+                        <Card>
+                            <div className='flex flex-column align-items-center'>
+                                <h1>Expense Tracker</h1>
+                                <img alt="logo" src={logo} height="100" width="100" />
+                                <div className="w-full">
+                                    <FloatLabel className="my-4">
+                                        <InputText id="username" className="w-full" value={loginUser.username} onChange={(e) => setLoginUser({ ...loginUser, username: e.target.value })} />
+                                        <label htmlFor="username">Username</label>
+                                    </FloatLabel>
+                                </div>
+                                <div className="w-full">
+                                    <FloatLabel className="my-4">
+                                        <Password inputId="password" feedback={false} className="w-full password-field" toggleMask value={loginUser.password} onChange={(e) => setLoginUser({ ...loginUser, password: e.target.value })} />
+                                        <label htmlFor="password">Password</label>
+                                    </FloatLabel>
+                                </div>
+                                <Button label="Login" onClick={onLogin} />
+                                <Toast ref={toast} />
+                            </div>
+                        </Card>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
-export default PersistLogin
