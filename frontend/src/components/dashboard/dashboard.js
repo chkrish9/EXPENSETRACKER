201c3dc246abf350673124a4c864be498bc075d9
Murali krishna sai Chukka <murali.24.4.91@gmail.com>
2024-06-08 02:15:26 -0500

    Update dashboard.js
    Number formatting

--- frontend/src/components/dashboard/dashboard.js
+++ frontend/src/components/dashboard/dashboard.js
@@ -1,184 +1,136 @@
-import React from "react";
-import {
-  bitcoin,
-  bank,
-  entertainment,
-  internet,
-  sendmoney,
-  car,
-  rent,
-  retirement,
-  previousmoney,
-  groceries,
-  book,
-  card,
-  circle,
-  food,
-  money,
-  piggy,
-  stocks,
-  takeaway,
-  tv,
-  gas,
-  trip,
-  health,
-  laundry,
-  mobile,
-  expensedefault,
-} from "../../utils/icons";
-import { numberFormat } from "../../utils/utilites";
-import { Button } from "primereact/button";
-import moment from "moment";
+import React, { useEffect, useState } from 'react'
+import { useGlobalContext } from '../../context/globalContext';
+import { DonutChart } from '../chart/donutChart';
+import { Knob } from 'primereact/knob';
+import { Card } from 'primereact/card';
+import { Calendar } from 'primereact/calendar';
+import { numberFormat } from '../../utils/utilites';
 
-export const TransactionBlock = ({
-  transaction,
-  deleteTransaction,
-  updateTransaction,
-  type,
-  categories,
-  subCategories,
-  accounts,
-}) => {
-  const categoryIcon = (category) => {
-    if (category === "other" && type === "income") {
-      category = "piggy";
-    } else if (category === "other" && type === "expense") {
-      category = "expensedefault";
-    }
-    switch (category) {
-      case "salary":
-        return money;
-      case "previous#month#money":
-        return previousmoney;
-      case "bank":
-      case "savings":
-        return bank;
-      case "401k":
-      case "hsa":
-        return retirement;
-      case "stocks":
-        return stocks;
-      case "crypto":
-        return bitcoin;
-      case "cash#back":
-      case "credit#card#bill#payment":
-      case "bofa-8653":
-      case "chase-0789":
-        return card;
-      case "piggy":
-        return piggy;
-      case "education":
-        return book;
-      case "internet":
-        return internet;
-      case "mobile":
-        return mobile;
-      case "send#money":
-        return sendmoney;
-      case "car":
-        return car;
-      case "food":
-        return food;
-      case "rent":
-        return rent;
-      case "groceries":
-        return groceries;
-      case "subscriptions":
-        return tv;
-      case "take#aways":
-        return takeaway;
-      case "entertainment":
-        return entertainment;
-      case "gas":
-        return gas;
-      case "trip":
-        return trip;
-      case "circle":
-        return circle;
-      case "health":
-        return health;
-      case "laundry":
-        return laundry;
-      default:
-        if (type === "income") {
-          return piggy;
-        } else if (type === "expense") {
-          return expensedefault;
-        }
-    }
-  };
-
-  return (
-    <div className="col-6 sm:col-4 lg:col-3 xl:col-2 p-2">
-      <div className="p-2 border-1 surface-border surface-card border-round">
-        <div className="flex flex-wrap align-items-center justify-content-center gap-2">
-          <div className="flex align-items-center gap-2">
-            {categories &&
-            transaction.category &&
-            subCategories &&
-            transaction.subCategory ? (
-              <>
-                <span className="text-2xl font-semibold">
-                  {categoryIcon(transaction.subCategory)}
-                </span>
-                <span className="hidden lg:flex xl:flex flex-column align-items-center">
-                  <span className="">
-                    {
-                      categories.find(
-                        (category) => category.code === transaction.category
-                      )?.name
-                    }
-                  </span>
-                  <span className="">
-                    (
-                    {
-                      subCategories.find(
-                        (category) => category.code === transaction.subCategory
-                      )?.name
-                    }
-                    )
-                  </span>
-                  <span className="">
-                    (
-                    {
-                      accounts.find(
-                        (account) => account.code === transaction.account
-                      )?.name
-                    }
-                    )
-                  </span>
-                </span>
-              </>
-            ) : (
-              ""
-            )}
-          </div>
-        </div>
-        <div className="flex flex-column align-items-center gap-3 py-5">
-          <span className="text-5xl">${numberFormat(transaction.amount)}</span>
-          <div className="font-bold">{transaction.title}</div>
-          <div className="font-bold">
-            <span className="pi pi-calendar"></span>
-            {moment(transaction.date).format("MM/DD/yyyy")}
-          </div>
-        </div>
-        <div className="flex justify-content-center">
-          <Button
-            icon="pi pi-pencil"
-            className="p-button-rounded mr-1"
-            severity="info"
-            onClick={() =>
-              updateTransaction(JSON.parse(JSON.stringify(transaction)))
+function Dashboard() {
+    const { totalExpenses, getIncomes, getExpenses, getTotalByCategories, totalIncome, dashboardDate,
+        setDashboardDate, incomes, expenses, totalBalanceInPercentage, setIncomesByMonthAndYear, setExpensesByMonthAndYear } = useGlobalContext();
+    const [incomePieChartData, setIncomePieChartData] = useState({
+        labels: [],
+        datasets: [
+            {
+                data: [],
             }
-          ></Button>
-          <Button
-            icon="pi pi-trash"
-            className="p-button-rounded"
-            severity="danger"
-            onClick={() => deleteTransaction(transaction._id)}
-          ></Button>
+        ]
+    });
+    const [expensePieChartData, setExpensePieChartData] = useState({
+        labels: [],
+        datasets: [
+            {
+                data: [],
+            }
+        ]
+    });
+
+    const maxDate = new Date();
+    const options = {
+        cutout: '80%'
+    };
+
+    const handleDate = async (date) => {
+        await setIncomesByMonthAndYear(date);
+        await setExpensesByMonthAndYear(date);
+        setDashboardDate(date);
+    }
+
+    useEffect(() => {
+        const data = getTotalByCategories(incomes, "category");
+        setIncomePieChartData({
+            labels: Object.keys(data),
+            datasets: [
+                {
+                    data: Object.values(data),
+                }
+            ]
+        });
+    }, [incomes]);
+
+    useEffect(() => {
+        const data = getTotalByCategories(expenses, "category");
+        setExpensePieChartData({
+            labels: Object.keys(data),
+            datasets: [
+                {
+                    data: Object.values(data),
+                }
+            ]
+        });
+    }, [expenses]);
+
+    useEffect(() => {
+        getIncomes()
+        getExpenses()
+        setIncomesByMonthAndYear(dashboardDate);
+        setExpensesByMonthAndYear(dashboardDate);
+    }, []);
+
+
+    return (
+        <div className="grid grid-nogutter">
+            <div className="col-12">
+                <div className="card m-3">
+                    <Card >
+                        <label>Month & Year : </label><Calendar value={dashboardDate} maxDate={maxDate} onChange={(e) => handleDate(e.value)} view="month" dateFormat="mm/yy" />
+                    </Card>
+                </div>
+            </div>
+            <div className="col-12 sm:col-12 lg:col-6 xl:col-6">
+                <div className="card m-3">
+                    <Card title="Remaining Balance">
+                        <div className='card flex flex-column justify-content-center align-items-center'>
+                            {
+                                 !isNaN(totalBalanceInPercentage()) ?  ((typeof totalBalanceInPercentage() === 'number' && totalBalanceInPercentage() >= 0 && totalBalanceInPercentage() <= 100) ?
+                                    <Knob value={numberFormat(totalBalanceInPercentage())} size={300} valueTemplate={'{value}%'} readOnly /> :
+                                    <span className={(totalBalanceInPercentage() > 100 ? 'positive':'negative') + " text-8xl my-8"}>{ numberFormat(totalBalanceInPercentage()) + "%" }</span>) : ""
+                            }
+                        </div>
+                    </Card>
+                </div>
+            </div>
+            <div className='col-12 sm:col-12 lg:col-6 xl:col-6'>
+                <div className="card m-3">
+                    <Card title="Individual Balances">
+                        <div className='flex flex-column justify-content-center align-items-center'>
+                            {
+                                totalIncome() ?
+                                    <div className='flex flex-column justify-content-center align-items-center'>
+                                        <h1>{numberFormat(totalIncome())}</h1>
+                                        <h2>Total Income</h2>
+                                    </div> : ""
+                            }
+                            {
+                                totalExpenses() ?
+                                    <div className='flex flex-column justify-content-center align-items-center'>
+                                        <h1>{numberFormat(totalExpenses())}</h1>
+                                        <h2>Total Expense</h2>
+                                    </div> : ""
+                            }
+                        </div>
+                    </Card>
+                </div>
+            </div>
+            <div className='col-12 sm:col-12 lg:col-6 xl:col-6'>
+                <div className="card m-3">
+                    <Card title="Income By Category">
+                        <DonutChart data={incomePieChartData} options={options} />
+                    </Card>
+                </div>
+            </div>
+            <div className='col-12 sm:col-12 lg:col-6 xl:col-6'>
+                <div className="card m-3">
+                    <Card title="Expense By Category">
+                        <DonutChart data={expensePieChartData} options={options} />
+                    </Card>
+                </div>
+            </div>
+
         </div>
-      </div>
-    </div>
-  );
-};
+    )
+}
+
+
+export default Dashboard
