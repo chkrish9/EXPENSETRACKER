7f3d139839640c4e582f33397628f5188a775a2c
Murali krishna sai Chukka <murali.24.4.91@gmail.com>
2024-06-08 02:14:02 -0500

    Update utilites.js
    Added number formatting

--- frontend/src/utils/utilites.js
+++ frontend/src/utils/utilites.js
@@ -1,41 +1,45 @@
+import { dashboard, expenses, trend } from "./icons";
 
-import {dashboard, expenses, trend} from './icons';
-
-const replaceChar = '#';
+const replaceChar = "#";
 const capitalizeFirstLetter = (string) => {
-    if(typeof string !== "string") return;
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
+  if (typeof string !== "string") return;
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
 
 export const getCode = (string) => {
-    if(typeof string !== "string") return;
-    return string.toLowerCase().replaceAll(' ', replaceChar);
-}
+  if (typeof string !== "string") return;
+  return string.toLowerCase().replaceAll(" ", replaceChar);
+};
 
 export const getName = (string) => {
-    if(typeof string !== "string") return;
-    return capitalizeFirstLetter(string).replaceAll(replaceChar, ' ');
-}
+  if (typeof string !== "string") return;
+  return capitalizeFirstLetter(string).replaceAll(replaceChar, " ");
+};
 
 export const getMenuItems = () => {
-    return [
-        {
-            id: 1,
-            title: 'Dashboard',
-            icon: dashboard,
-            link: '/dashboard'
-        },
-        {
-            id: 2,
-            title: "Incomes",
-            icon: trend,
-            link: "/incomes"
-        },
-        {
-            id: 3,
-            title: "Expenses",
-            icon: expenses,
-            link: "/expenses"
-        },
-    ]
-}
+  return [
+    {
+      id: 1,
+      title: "Dashboard",
+      icon: dashboard,
+      link: "/dashboard",
+    },
+    {
+      id: 2,
+      title: "Incomes",
+      icon: trend,
+      link: "/incomes",
+    },
+    {
+      id: 3,
+      title: "Expenses",
+      icon: expenses,
+      link: "/expenses",
+    },
+  ];
+};
+
+export const numberFormat = (number) => {
+  const formatter = new Intl.NumberFormat("en-US");
+  return formatter.format(number.toFixed(2));
+};
